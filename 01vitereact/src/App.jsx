7a6201cc81103015f376be54a6a0a89ebbd27e1a
novice-ismail <ismailcrt3@gmail.com>
2023-11-13 14:49:31 +0530

    react component using vite bundler

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>First react component with vite | IK</h1>
+    <p>
+      <ul>
+        <h2>Learnings:</h2>
+        <li>To create a react app using vite bundler the command is "npm create vite@latest".</li>
+        <li>In the next line need to name the project,then choose react , then using JavaScript.</li>
+        <li>Need to install node modules using the command "npm install/ npm i".</li>
+        <li>To run app on  server use command "npm run dev"(in development). </li>
+
+      </ul>
+    </p>
+    </>
+     
+  )
+}
+
+export default App
