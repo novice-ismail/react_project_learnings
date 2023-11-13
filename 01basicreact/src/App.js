da8897142302fec62990a73d2cc2fa3bf50b71d5
novice-ismail <ismailcrt3@gmail.com>
2023-11-13 14:34:53 +0530

    basic react component

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,29 @@
+
+
+
+function App() {
+  return (
+ <>
+   <h1>First react component | Ismail khan</h1>
+   <p><h2>Learnings:</h2>
+   React app can be built by using baic method and also by using bundlers like vite react or parcel.
+   <ul>
+    <li>
+    command to create by basic method is throgh "npx create-react-app-projectname".
+    </li>
+    <li>
+   Node modules are installed while creating the app itself.
+    </li>
+    <li>
+    command to create by vite method is throgh "npm create vite@latest", and in the next line it ask for project name.
+    </li>
+    <li>
+    Node modules need to be not available and need to be installed.
+    </li>
+   </ul>
+   </p>
+ </>
+  );
+}
+
+export default App;
