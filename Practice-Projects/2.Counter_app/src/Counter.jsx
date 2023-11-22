02bedd7409df6993c4005ec01af062b4c165c55f
novice-ismail <ismailcrt3@gmail.com>
2023-11-22 10:03:41 +0530

    Conter using useState

---
+++ Practice-Projects/2.Counter_app/src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './style.css';
+
+function Counter() {
+const [count, setCount] = useState(0)
+const increment = () => setCount(count + 1)
+const decrement = () => setCount(count - 1) 
+const reset = () => setCount(0) 
+
+  return (
+    <>
+    <div className="container">
+        <h1 className="number">{count}</h1>
+    </div>
+
+    <section className='btns-container'>
+        <button onClick={increment} className= "increment">+</button>
+        <button onClick={decrement} className= "decrement">-</button>
+        <button onClick={reset} className= "reset">Reset</button>
+    </section>
+    
+    
+    </>
+  )
+}
+
+export default Counter
