03d7c0b51ab345e57becbcff121ce3e31aefe643
novice-ismail <ismailcrt3@gmail.com>
2023-11-22 10:58:59 +0530

    Todolist-app

--- Practice-Projects/3.Todolist/src/App.jsx
+++ Practice-Projects/3.Todolist/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import Todo from './Todo'
 
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Todo />
       
     </>
   )
