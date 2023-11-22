782fe18000c2a6bc32a11e2b0a3bf55641fbe4a4
novice-ismail <ismailcrt3@gmail.com>
2023-11-22 09:07:46 +0530

    Counter using useReducer

---
+++ Practice-Projects/1.Counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useReducer } from 'react'
+
+import './App.css'
+
+const initialState = {count:0};
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case"increment":
+    return{
+      ...state,
+      count:state.count + 1,
+    }
+
+    case"decrement":
+    return{
+      ...state,
+      count:state.count - 1,
+    }
+    case"reset":
+    return{ 
+      ...state,
+      count:0,
+    }
+
+    default:
+    return state;
+  }
+}
+
+function App() {
+  
+
+  const [state, dispatch]  = useReducer(reducer, initialState)
+
+  
+
+  return (
+    <>
+    <h1>{state.count}</h1>
+    <button onClick={() => dispatch({type:"increment"})}>+</button>
+    <button onClick={() => dispatch({type:"decrement"})}>-</button>
+    <button onClick={() => dispatch({type:"reset"})}>Reset</button>
+     
+    </>
+  )
+}
+
+export default App
