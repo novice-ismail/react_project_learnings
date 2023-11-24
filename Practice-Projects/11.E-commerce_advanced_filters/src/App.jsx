80060089480b94a2ef1149e65101ece91f658ef3
novice-ismail <ismailcrt3@gmail.com>
2023-11-24 21:29:21 +0530

    E-commerce Advance Filtering project

---
+++ Practice-Projects/11.E-commerce_advanced_filters/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import Navigation from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
+import'./index.css'
+
+import products from './db/Data'
+import Card from './components/Card'
+
+function App() {
+
+  const[selectedCategory,setselectedCategory] = useState(null)
+  const[query,setQuery] = useState("")
+
+  // +++++++++++Input Filter+++++++++++++++++++++++++++++++++
+  
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter((product) =>
+  product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1) 
+
+  // +++++++++++Radio Filter+++++++++++++++++++++++++++++++++
+ 
+  const handleChange = (event) => {
+    setselectedCategory(event.target.value)
+  }
+
+ // +++++++++++Buttons Filter+++++++++++++++++++++++++++++++++
+ 
+ const handleClick = (event) => {
+  setselectedCategory(event.target.value)
+ }
+
+ function filteredData(products,selected,query){
+  let filteredProducts = products
+
+  // Filtering input Items
+ 
+  if(query){
+    filteredProducts = filteredItems;
+   
+  }
+
+// Selected Filter
+
+if(selected){
+  filteredProducts = filteredProducts.filter(
+    ({ category,color,company,newPrice,title })  => 
+    category === selected || 
+    color === selected || 
+    company === selected || 
+    newPrice === selected || 
+    title === selected
+    )
+}
+
+
+
+  return filteredProducts.map(({img, title, star, reviews, newPrice, prevPrice}) => (
+    <Card
+    key={Math.random()}
+    img={img}
+    title={title}
+    star={star}
+    reviews={reviews}
+    newPrice={newPrice}
+    prevPrice={prevPrice}
+
+    />
+  ))
+
+ }
+
+ const result = filteredData(products, selectedCategory, query)
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick}/>
+      <Products result={result} />
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ Practice-Projects/11.E-commerce_advanced_filters/src/Recommended/Recommended.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Recommended.css"
+import Buttons from '../components/Buttons'
+
+function Recommended({ handleClick }) {
+  return (
+    <>
+    <div>
+      <h2 className='recommended-title'>Recommended</h2>
+      <div className="recommended-flex">
+      
+      <Buttons onClickHandler={handleClick} value="" title="All Products"/>
+      <Buttons onClickHandler={handleClick} value="Nike" title="Nike"/>
+      <Buttons onClickHandler={handleClick} value="Adidas" title="adidas"/>
+      <Buttons onClickHandler={handleClick} value="Puma" title="Puma"/>
+      <Buttons onClickHandler={handleClick} value="Vans" title="Vans"/>
+      </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default Recommended
