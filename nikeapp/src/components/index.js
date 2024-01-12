127811d7c009dc1eead5cc935aadf8592e59160c
novice-ismail <ismailcrt3@gmail.com>
2024-01-12 12:30:07 +0530

    add finished nike website

---
+++ nikeapp/src/components/index.js
@@ -0,0 +1,19 @@
+import Nav from './Nav'
+import Button from './Button'
+import ShoeCard from './ShoeCard'
+import PopularProductCard from './PopularProductCard'
+import ServiceCard from './ServiceCard'
+import ReviewCard from './ReviewCard'
+
+
+
+
+
+export {
+    Nav,
+    Button,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard
+}
